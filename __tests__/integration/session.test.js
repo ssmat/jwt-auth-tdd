e7fd1dc4b177c6a9255a8a28ea881b51b25bbc3e
mateus <admmateus@outlook.com>
2020-07-11 16:41:54 -0300

    Created my first TDD project for study purposes

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,76 @@
+const request = require("supertest");
+const app = require("../../src/app");
+const factory = require("../factories");
+const truncate = require("../utils/truncate");
+
+describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it("should authenticate with valid credentials", async () => {
+    const user = await factory.create("User", {
+      password: "1q2w3e4r",
+    });
+
+    console.log(user);
+
+    const response = await request(app).post("/sessions").send({
+      email: user.email,
+      password: "1q2w3e4r",
+    });
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should not authenticate with invalid credentials", async () => {
+    const user = await factory.create("User", {
+      password: "1q2w3e4r",
+    });
+
+    const response = await request(app).post("/sessions").send({
+      email: user.email,
+      password: "23423423",
+    });
+
+    expect(response.status).toBe(401);
+  });
+
+  it("should return jwt token when authenticated", async () => {
+    const user = await factory.create("User", {
+      password: "1q2w3e4r",
+    });
+
+    const response = await request(app).post("/sessions").send({
+      email: user.email,
+      password: "1q2w3e4r",
+    });
+
+    expect(response.body).toHaveProperty("token");
+  });
+
+  it("should be able to access private routes when authenticated", async () => {
+    const user = await factory.create("User", {
+      password: "1q2w3e4r",
+    });
+
+    const response = await request(app)
+      .get("/dashboard")
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should not be able to access private routes without jwt token", async () => {
+    const response = await request(app).get("/dashboard");
+    expect(response.status).toBe(401);
+  });
+
+  it("should not be able to access private routes with invalid jwt token", async () => {
+    const response = await request(app)
+      .get("/dashboard")
+      .set("Authorization", `Bearer 123123`);
+
+    expect(response.status).toBe(401);
+  });
+});
